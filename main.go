9f800c7f2046cd66aaf217007fd92837ed1edbf6
cicatellid13 <dcicatelli100@gmail.com>
2025-06-13 21:21:14 -0400

    removed commented out code

--- main.go
+++ main.go
@@ -3,21 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	// "os"
 )
 
 func main() {
 	mux := http.NewServeMux()
-
-	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	data, err := os.ReadFile("index.html")
-	// 	if err != nil {
-	// 		http.Error(w, "Could not read index.html", http.StatusInternalServerError)
-	// 		return
-	// 	}
-	// 	w.Header().Set("Content-Type", "text/html")
-	// 	w.Write(data)
-	// })
 	mux.Handle("/", http.FileServer(http.Dir(".")))
 
 	server := &http.Server{
